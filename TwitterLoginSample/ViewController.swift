a188e6c23e907203921e7181351a40e8319a4079
otukutun <orehaorz@gmail.com>
2017-01-05 15:16:50 +0900

    試しにAccountフレームワークを触ってみる.

--- TwitterLoginSample/ViewController.swift
+++ TwitterLoginSample/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Accounts
+import Social
 
 class ViewController: UIViewController {
 
+    var accountStore:ACAccountStore = ACAccountStore()
+    var twAccount: ACAccount?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func signInTwitterAccount(_ sender: Any) {
+        self.selectTwitterAccount()
+    }
 
-
+    func selectTwitterAccount() {
+        
+        let accountType:ACAccountType = accountStore.accountType(withAccountTypeIdentifier: ACAccountTypeIdentifierTwitter)
+        accountStore.requestAccessToAccounts(with: accountType, options: nil) { (granted: Bool, error: Error?) -> Void in
+            if error != nil {
+                print("error! \(error)")
+                return
+            }
+            
+            if !granted {
+                print("error! Twitterアカウントの利用が許可されていません")
+                return
+            }
+            
+            let accounts = self.accountStore.accounts(with: accountType) as! [ACAccount]
+            
+            if accounts.count == 0 {
+                print("error! 設定画面からアカウントを設定してください")
+                return
+            }
+            
+            print("アカウント取得完了")
+        }
+    }
 }
 
